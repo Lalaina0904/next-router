0dff42edfa111d50a0134b3f6b2967d29563454c
Lalaina0904 <lalainap19.aps2b@gmail.com>
2024-03-10 22:03:28 +0300

    feat : npm i tailwind pkg

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./pages/**/*.{ts,tsx}",
+        "./components/**/*.{ts,tsx}",
+        "./app/**/*.{ts,tsx}",
+        "./src/**/*.{ts,tsx}",
+        "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
